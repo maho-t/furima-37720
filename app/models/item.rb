35e0e56113a6d4f1abc747ba51a0a4b5a97b06fe
pipimaho <mtateno0@gmail.com>
2022-04-25 18:36:32 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :delivery_days
 end

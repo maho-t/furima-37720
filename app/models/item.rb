2e762f47ea1b22d00ae752d7478a61a715f3e798
pipimaho <mtateno0@gmail.com>
2022-04-25 18:56:32 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,28 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_days_id, presence: true
+  with_options presence, true, format { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :charge
   belongs_to :prefecture
   belongs_to :delivery_days
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :charge_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :delivery_days_id, numericality: { other_than: 1 } 
+
 end

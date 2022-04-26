23bc2cb4054eb23df777598d810b301bc7cc8884
pipimaho <mtateno0@gmail.com>
2022-04-26 13:01:08 +0900

    入力ごとにイベント発火をさせる

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', () => {
 
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火")
+  })
 
 })
-
